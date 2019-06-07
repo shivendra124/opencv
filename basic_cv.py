6a6c8ebf04487170b01a7bf8a8ac24e567a382bd
shivendra124 <shivendra124124@gmail.com>
2019-06-07 12:17:27 +0530

    minor change

--- basic_cv.py
+++ basic_cv.py
@@ -6,7 +6,7 @@ while True:
     a = a + 1
     check, frame = video.read()
     print(frame)
-    gray, i = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray, a, b = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Capture", gray)
     key = cv2.waitKey(1)
     if key == ord('q'):
